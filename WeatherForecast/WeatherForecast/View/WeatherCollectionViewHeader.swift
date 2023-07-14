fd7aacdc8cb310bd2c9471a2880fe466bd00986a
Lust3r <s1skyblue1s@gmail.com>
2023-07-14 21:40:04 +0900

    refactor: #2 CollectionView Header 구성 변경
    - 기존: 모든 UI 요소가 제약을 통해 배열
    - 변경: 위치, 기온 관련 라벨을 stackView로 관리

--- WeatherForecast/WeatherForecast/View/WeatherCollectionViewHeader.swift
+++ WeatherForecast/WeatherForecast/View/WeatherCollectionViewHeader.swift
@@ -14,6 +14,7 @@ class WeatherCollectionViewHeader: UICollectionReusableView {
 
     // MARK: - Public property
     var weatherImage = UIImageView()
+    var informationStack = UIStackView()
     var locationLabel = UILabel()
     var tempMinAndMaxLabel = UILabel()
     var tempLabel = UILabel()
@@ -24,10 +25,12 @@ class WeatherCollectionViewHeader: UICollectionReusableView {
         super.init(frame: frame)
 
         addSubview(weatherImage)
-        addSubview(locationLabel)
-        addSubview(tempMinAndMaxLabel)
-        addSubview(tempLabel)
-        
+        addSubview(informationStack)
+
+        informationStack.addArrangedSubview(locationLabel)
+        informationStack.addArrangedSubview(tempMinAndMaxLabel)
+        informationStack.addArrangedSubview(tempLabel)
+
         configureConstraints()
         configureUI()
     }
@@ -40,21 +43,25 @@ class WeatherCollectionViewHeader: UICollectionReusableView {
     private func configureUI() {
         weatherImage.contentMode = .scaleAspectFit
 
+        informationStack.axis = .vertical
+        informationStack.spacing = 5
+        informationStack.setCustomSpacing(15, after: tempMinAndMaxLabel)
+        informationStack.alignment = .leading
+        informationStack.distribution = .fillProportionally
+
         locationLabel.textColor = .white
         locationLabel.font = UIFont.systemFont(ofSize: 15)
-        
+
         tempMinAndMaxLabel.textColor = .white
         tempMinAndMaxLabel.font = UIFont.systemFont(ofSize: 15)
-        
+
         tempLabel.textColor = .white
         tempLabel.font = UIFont.systemFont(ofSize: 30)
     }
-    
+
     private func configureConstraints() {
         weatherImage.translatesAutoresizingMaskIntoConstraints = false
-        locationLabel.translatesAutoresizingMaskIntoConstraints = false
-        tempMinAndMaxLabel.translatesAutoresizingMaskIntoConstraints = false
-        tempLabel.translatesAutoresizingMaskIntoConstraints = false
+        informationStack.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             weatherImage.topAnchor.constraint(equalTo: self.topAnchor),
@@ -63,21 +70,9 @@ class WeatherCollectionViewHeader: UICollectionReusableView {
             weatherImage.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             weatherImage.widthAnchor.constraint(equalToConstant: self.frame.width * 0.25),
 
-            locationLabel.topAnchor.constraint(equalTo: self.topAnchor),
-            locationLabel.leadingAnchor.constraint(equalTo: weatherImage.trailingAnchor),
-            locationLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            locationLabel.heightAnchor.constraint(equalToConstant: self.frame.height / 5),
-
-            tempMinAndMaxLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor),
-            tempMinAndMaxLabel.leadingAnchor.constraint(equalTo: weatherImage.trailingAnchor),
-            tempMinAndMaxLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            tempMinAndMaxLabel.heightAnchor.constraint(equalToConstant: self.frame.height / 5),
-
-            tempLabel.topAnchor.constraint(equalTo: tempMinAndMaxLabel.bottomAnchor),
-            tempLabel.leadingAnchor.constraint(equalTo: weatherImage.trailingAnchor),
-            tempLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            tempLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            tempLabel.heightAnchor.constraint(equalToConstant: self.frame.height * 3 / 5)
+            informationStack.topAnchor.constraint(equalTo: self.topAnchor),
+            informationStack.leadingAnchor.constraint(equalTo: weatherImage.trailingAnchor),
+            informationStack.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
     }
 }
